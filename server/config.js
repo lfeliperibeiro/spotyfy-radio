1dec37b5003714b8f9d6e79cfa756b5d13a4173d
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-17 22:26:38 -0300

    feat: create normalizes

---
+++ server/config.js
@@ -0,0 +1,22 @@
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const root = join(currentDir, '../')
+const audioDirectory = join(root, 'audio')
+const publicDirectory = join(root, 'puclic')
+
+export default {
+    port: process.env.PORT || 3000,
+    dir: {
+        root,
+        publicDirectory,
+        audioDirectory,
+        songsDirectory: join(audioDirectory, 'songs'),
+        fxDirectory: join(audioDirectory, 'fx')
+    },
+    pages: {
+        homeHTML: 'home/index.html',
+        controllerFTML: 'controller/index.html',
+    },
+    location: '/home'
+}
