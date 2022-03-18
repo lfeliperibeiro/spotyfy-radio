e604c213a87add9c82a7a3d6fdc3e4e2e76ccdd4
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-17 22:54:24 -0300

    chore: run prettier

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,10 @@
-import {Service} from "./service.js";
+import { Service } from './service.js';
 
 export class Controller {
-    constructor() {
-        this.service = new Service
-    }
-    async getFileStream(filename){
-        return this.service.getFileStream(filename)
-    }
+  constructor() {
+    this.service = new Service();
+  }
+  async getFileStream(filename) {
+    return this.service.getFileStream(filename);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-import server from "./server.js";
-import {logger} from "./util.js";
-import config from "./config.js";
+import server from './server.js';
+import { logger } from './util.js';
+import config from './config.js';
 
-
-
-server.listen(config.port)
-    .on('listening', () => logger.info(`server running at ${config.port}!!`));
+server
+  .listen(config.port)
+  .on('listening', () => logger.info(`server running at ${config.port}!!`));
